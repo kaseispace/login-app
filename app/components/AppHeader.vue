a1c0d8634ae49f76e8e6e90cc1f8b4078af78d4b
yuki0829 <fledgling0829@gmail.com>
2026-02-12 15:16:44 +0900

    feat: ヘッダーコンポーネントを新規追加

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const client = useSupabaseClient()
+const user = useSupabaseUser()
+
+const logout = async () => {
+  await client.auth.signOut()
+  navigateTo('/login')
+}
+</script>
+
+<template>
+  <UHeader :toggle="false">
+    <template #right>
+      <UColorModeButton variant="link" />
+
+      <UButton
+        v-if="user"
+        variant="link"
+        color="primary"
+        @click="logout"
+      >
+        Logout
+      </UButton>
+    </template>
+  </UHeader>
+</template>
