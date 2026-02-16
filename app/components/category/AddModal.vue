b8ddd84471b989d541671d4ec26cdffc1ccdac10
yuki0829 <fledgling0829@gmail.com>
2026-02-16 22:07:39 +0900

    feat: カテゴリ作成モーダルを実装

---
+++ app/components/category/AddModal.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { createCategorySchema, ApiErrorSchema } from '#schemas/index'
+import type { CreateCategory } from '#schemas/index'
+import type { FormSubmitEvent } from '@nuxt/ui'
+
+const { refresh, createCategory } = useCategories()
+const { showSuccess, showWarning, showError } = useAppToast()
+
+const state = reactive<CreateCategory>({
+  title: '',
+})
+
+const open = ref(false)
+
+defineShortcuts({
+  o: () => open.value = !open.value,
+})
+
+const onSubmit = async (event: FormSubmitEvent<CreateCategory>) => {
+  try {
+    await createCategory(event.data)
+    await refresh()
+    showSuccess('カテゴリを追加しました', { progress: false })
+    state.title = ''
+    open.value = false
+  }
+  catch (err) {
+    const parsed = ApiErrorSchema.safeParse(err)
+
+    if (parsed.success) {
+      const error = parsed.data
+      showWarning(error.data.message || 'カテゴリの追加に失敗しました', { progress: false })
+    }
+    else {
+      showError('予期しないエラーが発生しました', { progress: false })
+    }
+  }
+}
+</script>
+
+<template>
+  <UModal v-model:open="open" title="新しいカテゴリを作成" description="カテゴリ名を入力してください。">
+    <UButton
+      icon="i-lucide-pen"
+      label="カテゴリを追加"
+      color="secondary"
+      variant="subtle"
+    />
+
+    <template #body>
+      <UForm :schema="createCategorySchema" :state="state" @submit="onSubmit">
+        <UFormField label="カテゴリ名" name="title">
+          <UInput v-model="state.title" class="w-full" color="neutral" />
+        </UFormField>
+
+        <div class="flex justify-end mt-3">
+          <UButton type="submit" color="secondary">
+            作成
+          </UButton>
+        </div>
+      </UForm>
+    </template>
+  </UModal>
+</template>
